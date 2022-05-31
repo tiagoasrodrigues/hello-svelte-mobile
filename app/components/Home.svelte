0b1fd1536c83e4d96bcf2390319f2d4ed48d7b58
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-31 20:12:35 -0300

    :wrench: Add actionBar with actionItem

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
   import Navbar from "./NavBar.svelte";
   import { navigate } from "svelte-native";
+
+  function onTapShare() {
+    alert('Share!');
+  }
+
+  function onTapDelete() {
+    alert('Delete!');
+  }
+
 </script>
 
 <page>
-    <actionBar flat="true">
-        <stackLayout orientation="horizontal">
-            <label text="Home" fontSize="24" verticalAlignment="middle" />
-            <label text="About" fontSize="24" verticalAlignment="middle" />
-        </stackLayout>
-    </actionBar>
+  <actionBar title="Home">
+    <actionItem on:tap="{onTapShare}" android.systemIcon="ic_menu_search" android.position="actionBar" />
+    <actionItem on:tap="{onTapDelete}" android.systemIcon="ic_menu_share" android.position="actionBar" />
+  </actionBar>
 
   <stackLayout>
-    <button
-      text="Navbar"
-      on:tap={() => navigate({ page: Navbar })}
-    />
-    <stackLayout />
+    <button text="Navbar" on:tap={()=> navigate({ page: Navbar })}
+      />
+      <stackLayout />
   </stackLayout>
-  </page>
+</page>
 
 <style>
- 
+
 </style>
