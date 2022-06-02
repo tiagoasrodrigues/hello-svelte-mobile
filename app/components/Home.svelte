9a7bfb950eb556bbf71de880a1fe1e45ab784ac2
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-06-02 10:37:54 -0300

    :wrench: Add navigationButton

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -2,29 +2,54 @@
   import Navbar from "./NavBar.svelte";
   import { navigate } from "svelte-native";
 
+  function onTapSettings() {
+    alert('Settings!');
+  }
+
   function onTapShare() {
     alert('Share!');
   }
 
-  function onTapDelete() {
-    alert('Delete!');
+  function onTapEdit() {
+    alert('Edit!');
   }
 
+  function onTapGoBack() {
+    alert('Go back!');
+  }
 </script>
 
 <page>
-  <actionBar title="Home">
-    <actionItem on:tap="{onTapShare}" android.systemIcon="ic_menu_search" android.position="actionBar" />
-    <actionItem on:tap="{onTapDelete}" android.systemIcon="ic_menu_share" android.position="actionBar" />
+  <actionBar class="appBar">
+
+    <stackLayout>
+      <label text="Obra - Obra" />
+    </stackLayout>
+
+    <navigationButton on:tap="{onTapGoBack}" android.systemIcon="ic_menu_back" android.position="actionBar" />
+    <actionItem on:tap="{onTapSettings}" android.systemIcon="ic_menu_preferences" android.position="actionBar" />
+    <actionItem on:tap="{onTapShare}" android.systemIcon="ic_menu_share" android.position="actionBar" />
+    <actionItem on:tap="{onTapEdit}" android.systemIcon="ic_menu_edit" android.position="actionBar" />
   </actionBar>
 
   <stackLayout>
-    <button text="Navbar" on:tap={()=> navigate({ page: Navbar })}
+    <button text="ATERRO DE RESÍDUOS" on:tap={()=> navigate({ page: Navbar })}
       />
-      <stackLayout />
+      <button text="SMO" on:tap={()=> navigate({ page: Navbar })}
+        />
+        <button text="TOPOGRÁFICO" on:tap={()=> navigate({ page: Navbar })}
+          />
+          <stackLayout />
   </stackLayout>
 </page>
 
 <style>
+  .appBar {
+    background-color: #388E3C;
+  }
 
+  label {
+    color: white;
+    font-size: 20;
+  }
 </style>
