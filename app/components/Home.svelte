b2cf9cbb4436c65887099342933d0f44fc9ec196
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-06-15 10:44:23 -0300

    :wrench: Bluetooth connect

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -2,6 +2,7 @@
   import Navbar from "./NavBar.svelte";
   import { navigate } from "svelte-native";
   import { Bluetooth } from '@nativescript-community/ble';
+  import { Template } from 'svelte-native/components'
 
   function onTapSettings() {
     alert('Settings!');
@@ -17,6 +18,12 @@
 
   function onTapGoBack() {
     alert('Go back!');
+  }
+
+  let peripherals = ["one", 'two', 'three']
+
+  function onPeripheralTap() {
+    alert("Peripheral found with UUID: ");
   }
 
   var bluetooth = new Bluetooth();
@@ -39,6 +46,18 @@
   }, function (err) {
     console.log("error while scanning: " + err);
   });
+
+  bluetooth.connect({
+     UUID: 'EA:D8:D1:1B:29:A5',
+    onConnected: function (peripheral) {
+      console.log("Peripheral connected with UUID: " + peripheral.UUID);
+      alert("Peripheral connected with UUID: " + peripheral.UUID);
+      // the peripheral object now has a list of available services:
+      peripheral.services.forEach(function (service) {
+        console.log("service found: " + JSON.stringify(service));
+      });
+    },
+  });
 </script>
 
 <page>
@@ -53,6 +72,13 @@
     <actionItem on:tap="{onTapShare}" android.systemIcon="ic_menu_share" android.position="actionBar" />
     <actionItem on:tap="{onTapEdit}" android.systemIcon="ic_menu_edit" android.position="actionBar" />
   </actionBar>
+
+  <listView items="{ peripherals }" on:itemTap="{onPeripheralTap}">
+    <Template let:item>
+      <!-- Shows the list item label in the default color and style. -->
+      <label text="{item}" />
+    </Template>
+  </listView>
 </page>
 
 <style>
